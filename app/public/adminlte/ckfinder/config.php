1def2a5fbdd51911698ef84f220e260f666f1179
tektolog <221faza@gmail.com>
2025-10-22 20:47:16 +0300

    test 4

--- app/public/adminlte/ckfinder/config.php
+++ app/public/adminlte/ckfinder/config.php
@@ -1,6 +1,6 @@
 <?php
 
 const DEBUG = true;
-const DB_USER = 'user55555';
-const DB_PASS = 'password55555';
-const DB_HOST = 'mysql55555';
+const DB_USER = 'user77777';
+const DB_PASS = 'password7777';
+const DB_HOST = 'mysql88888';
