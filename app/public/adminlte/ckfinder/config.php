1f189b050b3436c0d938b7c1136b78039fbafee1
tektolog <221faza@gmail.com>
2025-10-23 00:08:33 +0300

    commit fix

--- app/public/adminlte/ckfinder/config.php
+++ app/public/adminlte/ckfinder/config.php
@@ -1,6 +1,6 @@
 <?php
 
 const DEBUG = true;
-const DB_USER = 'user2222222';
+const DB_USER = 'user33333333';
 const DB_PASS = 'password8888';
-const DB_HOST = 'mysql88888';
+// const DB_HOST = 'mysql88888';
