fb91aaa4c59e8bf381644761782d2785508cae96
tektolog <221faza@gmail.com>
2025-10-22 20:20:46 +0300

    new deploy view

--- app/public/adminlte/ckfinder/config.php
+++ app/public/adminlte/ckfinder/config.php
@@ -1,6 +1,6 @@
 <?php
 
 const DEBUG = true;
-const DB_USER = 'user2';
-const DB_PASS = 'password2';
-const DB_HOST = 'mysql2';
+const DB_USER = 'user33332';
+const DB_PASS = 'password33332';
+const DB_HOST = 'mysql33332';
